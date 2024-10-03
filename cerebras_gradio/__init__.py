8311587f39bb96f9aedd7fdf7623b5541cc40f24
Abubakar Abid <abubakar@huggingface.co>
2024-10-03 13:09:48 -0700

    style

--- cerebras_gradio/__init__.py
+++ cerebras_gradio/__init__.py
@@ -7,15 +7,10 @@ __version__ = "0.0.1"
 
 def get_fn(model_name: str, preprocess: Callable, postprocess: Callable, api_key: str):
     def fn(message, history):
-        # Preprocess the inputs
         inputs = preprocess(message, history)
-
-        # Initialize the Cerebras client for Cerebras
         client = Cerebras(
-            api_key=api_key, #os.environ.get("CEREBRAS_API_KEY"),
+            api_key=api_key,
         )
-
-        # Call the Cerebras Client with streaming enabled
         completion = client.chat.completions.create(
             model=model_name,
             messages=inputs['messages'],
@@ -32,11 +27,9 @@ def get_fn(model_name: str, preprocess: Callable, postprocess: Callable, api_key
 
 def get_interface_args(pipeline):
     if pipeline == "chat":
-        # Using the default ChatInterface for chat models
         inputs = None
         outputs = None
         def preprocess(message, history):
-            # Constructing the messages list from the conversation history
             messages = []
             for user_msg, assistant_msg in history:
                 messages.append({"role": "user", "content": user_msg})
@@ -44,9 +37,8 @@ def get_interface_args(pipeline):
             messages.append({"role": "user", "content": message})
             return {'messages': messages}
 
-        postprocess = lambda x: x  # No post-processing needed
+        postprocess = lambda x: x
     else:
-        # Add other pipeline types when they will be needed
         raise ValueError(f"Unsupported pipeline type: {pipeline}")
     return inputs, outputs, preprocess, postprocess
 
@@ -63,21 +55,18 @@ def registry(name: str, token: str | None = None, **kwargs):
         - name (str): The name of the model on Cerebras.
         - token (str, optional): The API key for Cerebras.
     """
-    # Ensure the Cerebras API key is set
     api_key = token or os.environ.get("CEREBRAS_API_KEY")
     if not api_key:
         raise ValueError("CEREBRAS_API_KEY environment variable is not set.")
 
-    # Determine the pipeline type
     pipeline = get_pipeline(name)
     inputs, outputs, preprocess, postprocess = get_interface_args(pipeline)
     fn = get_fn(name, preprocess, postprocess, api_key)
 
     if pipeline == "chat":
-        # Create a Gradio ChatInterface
         interface = gr.ChatInterface(fn=fn, **kwargs)
     else:
-        # For other pipelines, create a standard Interface
+        # For other pipelines, create a standard Interface (not implemented yet)
         interface = gr.Interface(fn=fn, inputs=inputs, outputs=outputs, **kwargs)
 
     return interface
